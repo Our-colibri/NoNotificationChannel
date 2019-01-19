e1b5099b471f694d642a7bb606d3fb6ae1adb3a3
hnn.addict <hnn.addict@gmail.com>
2019-01-19 16:18:06 +0900

    [change] "Hidden api" restriction evaded from Android P.

--- no-notification-channel/src/main/java/jp/kemomimi/util/NoNotificationChannel.java
+++ no-notification-channel/src/main/java/jp/kemomimi/util/NoNotificationChannel.java
@@ -12,6 +12,8 @@ import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.List;
 
+import android.support.annotation.RequiresApi;
+
 /**
  * Created by shintani on 2017/06/23.
  */
@@ -32,16 +34,11 @@ public class NoNotificationChannel {
     }
 
     @TargetApi(Build.VERSION_CODES.O)
-    public static void wrappNotify(Context context, int id, Notification notification, String channelName){
-        NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        wrappNotify(manager, id, notification, channelName);
-    }
-
-    @TargetApi(Build.VERSION_CODES.O)
     public static void wrappNotify(Context context, int id, Notification notification){
         wrappNotify(context, id, notification, "Notification");
     }
 
+    @Deprecated
     @TargetApi(Build.VERSION_CODES.O)
     public static void wrappNotify(NotificationManager manager, int id, Notification notification){
         wrappNotify(manager, id, notification, "Notification");
@@ -112,5 +109,79 @@ public class NoNotificationChannel {
 
         }
 
+    }
+
+    @TargetApi(Build.VERSION_CODES.O)
+    public static void wrappNotify(Context context, int id, Notification notification, String channelName){
+
+        NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            String genid = String.valueOf(String.valueOf(notification.ledARGB) +
+                    (notification.sound != null?notification.sound.toString():"no sound") +
+                    (notification.vibrate != null?Arrays.toString(notification.vibrate):"no vibrate") +
+                    notification.visibility + notification.getBadgeIconType() + notification.flags);
+
+            String channelid = android.util.Base64.encodeToString(genid.getBytes(), android.util.Base64.URL_SAFE);
+
+            notification = reCreateNotification(context, channelid, notification);
+
+            List<NotificationChannelGroup> ncgs = manager.getNotificationChannelGroups();
+            if(ncgs != null){
+                for(NotificationChannelGroup g:ncgs){
+                    if(g.getId().equals(CHANNELGROUPID)){
+                        manager.deleteNotificationChannelGroup(CHANNELGROUPID);
+                        break;
+                    }
+                }
+            }
+
+            NotificationChannelGroup ncg = new NotificationChannelGroup(CHANNELGROUPID, mGroupName);
+            NotificationChannel nc = new NotificationChannel(channelid,
+                    channelName, mPrio);
+
+            nc.setLockscreenVisibility(notification.visibility);
+            nc.enableLights((notification.flags & Notification.FLAG_SHOW_LIGHTS) == 1);
+            if(notification.vibrate != null) {
+                nc.enableVibration(true);
+                nc.setVibrationPattern(notification.vibrate);
+            }
+            nc.setLightColor(notification.ledARGB);
+            if(notification.sound != null) {
+                nc.setSound(notification.sound, nc.getAudioAttributes());
+            }
+            nc.setShowBadge(notification.getBadgeIconType() != Notification.BADGE_ICON_NONE);
+            nc.setGroup(CHANNELGROUPID);
+
+            manager.createNotificationChannelGroup(ncg);
+            manager.createNotificationChannel(nc);
+
+            manager.notify(id, notification);
+
+        } else {
+
+            manager.notify(id, notification);
+
+        }
+
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private static Notification reCreateNotification(Context context, String channelid, Notification srcNotification ){
+        Notification.Builder builder = new Notification.Builder(context, channelid);
+        if(srcNotification.actions != null)builder.setActions(srcNotification.actions);
+        builder.setAutoCancel((srcNotification.flags & Notification.FLAG_AUTO_CANCEL) != 0);
+        builder.setBadgeIconType(srcNotification.getBadgeIconType());
+
+        builder.setTicker(srcNotification.tickerText);
+        builder.setSmallIcon(srcNotification.getSmallIcon());
+        builder.setWhen(srcNotification.when);
+        //contentText
+        //contentTitle
+        //showWhen
+        builder.setExtras(srcNotification.extras);
+        builder.setCategory(srcNotification.category);
+        builder.setContentIntent(srcNotification.contentIntent);
+
+        return builder.build();
     }
 }
